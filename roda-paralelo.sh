6b8e22c530f716a7742286c0881d4184bbb358d1
Bruna Camily <bcdnovack@inf.ufpel.edu.br>
2026-02-21 13:04:01 -0300

    Organização dos resultados e limpeza de arquivos antigos

--- roda-paralelo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-python3 iris.py >> results/saida-iris.txt &
-
--- roda_mistas.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python3 seg_otim.py mycin 9 100 >> mistas/results/mycin/saida-9_100.txt &
